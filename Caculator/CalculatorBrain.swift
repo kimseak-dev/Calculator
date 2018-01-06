2c5d60fc47fa7ed82999880668379f4be0391d99
kimseak dev <kimseakphon@gmail.com>
2018-01-06 13:41:09 +0700

    calculator standford

--- Caculator/CalculatorBrain.swift
+++ Caculator/CalculatorBrain.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+struct CalculatorBrain {
+//
+//    private var accumulator: Double?
+//    func performOperation(_ sysmbol:String){
+//        switch mathematicalSymbol{
+//        case "π":
+//            display!.text = String(Double.pi)
+//        case "√":
+//            displayValue = sqrt(displayValue)
+//        default:
+//            break
+//        }
+//
+//    }
+//
+//    mutating func setOperand(_ operand: Double){
+//        accumulator = operand
+//    }
+//    
+//    var result: Double?{
+//        get{
+//            return accumulator
+//        }
+//    }
+
+}
--- Caculator/ViewController.swift
+++ Caculator/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+       
+        if(userIsTheMiddleOfTyping){
+             let textCurrentlyInDisplay = display.text!
+             display!.text = textCurrentlyInDisplay + digit
+        }else{
+            display!.text = digit
+            userIsTheMiddleOfTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        get{
+          return Double(display.text!)!
+        }
+        set{
+            display?.text = String(newValue)
+        }
     }
+    
+    private var brain:CalculatorBrain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle{
+            switch mathematicalSymbol{
+            case "π":
+                display!.text = String(Double.pi)
+            case "√":
+                //let operand = Double(display!.text!)
+                //display!.text = String(sqrt(operand!))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
 
-
+        
+    }
+    
 }
 
